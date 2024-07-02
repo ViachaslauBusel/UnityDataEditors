1547997648f9aacc56653f4b17c12efc26cfbaf9
DESKTOP-P4LPPRA\Viachaslau <vyacheslav.bysel@gmail.com>
2024-07-02 23:07:37 +0200

    - Moved ItemDetailsAttribute from the editor assembly to the runtime assembly.

--- Assets/Scripts/ExTestData.cs
+++ Assets/Scripts/ExTestData.cs
@@ -1,9 +1,4 @@
-﻿using ObjectRegistryEditor.SelectorWindow;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ObjectRegistryEditor;
 
 namespace Assets.Scripts
 {
--- Packages/com.object.registry.editor/Runtime/ItemDetailsAttribute.cs
+++ Packages/com.object.registry.editor/Runtime/ItemDetailsAttribute.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ObjectRegistryEditor.SelectorWindow
+namespace ObjectRegistryEditor
 {
     public class ItemDetailsAttribute : Attribute
     {
